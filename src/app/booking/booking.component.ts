193f1605ca890fd037be993643b5a42da43960a1
HabeebR07 <habeebrahmanca07@gmail.com>
2024-10-28 10:15:13 +0530

    reactive form validation

--- src/app/booking/booking.component.ts
+++ src/app/booking/booking.component.ts
@@ -1,17 +1,18 @@
 import { CommonModule } from '@angular/common';
 import { Component, OnInit } from '@angular/core';
-import { FormArray, FormBuilder, FormControl, FormGroup, ReactiveFormsModule } from '@angular/forms';
+import { FormArray, FormBuilder, FormControl, FormGroup, ReactiveFormsModule, Validators } from '@angular/forms';
 import { ConfigService } from '../services/config.service';
 import { MatFormFieldModule } from '@angular/material/form-field';
 import { MatInputModule } from '@angular/material/input';
 import { MatButtonModule } from '@angular/material/button';
 import { MatExpansionModule } from '@angular/material/expansion';
 import { MatIconModule } from '@angular/material/icon';
+import { MatCheckboxModule } from '@angular/material/checkbox';
 
 @Component({
   selector: 'app-booking',
   standalone: true,
-  imports: [ReactiveFormsModule, CommonModule, MatFormFieldModule, MatInputModule, MatButtonModule, MatExpansionModule, MatIconModule],
+  imports: [ReactiveFormsModule, CommonModule, MatFormFieldModule, MatInputModule, MatButtonModule, MatExpansionModule, MatIconModule, MatCheckboxModule],
   templateUrl: './booking.component.html',
   styleUrl: './booking.component.css'
 })
@@ -27,27 +28,40 @@ export class BookingComponent implements OnInit {
 
   ngOnInit(): void {
     this.bookingForm = this.formBuilder.group({
-      roomId: new FormControl({ value: '2', disabled: true }),
-      guestEmail: new FormControl(''),
+      roomId: new FormControl({ value: '1', disabled: false }, { validators: [Validators.required] }),
+      guestEmail: new FormControl('',{updateOn: 'blur', validators: [Validators.required, Validators.email] }),
       checkIn: new FormControl(''),
       checkOut: new FormControl(''),
       bookingStatus: new FormControl(''),
       bookingAmount: new FormControl(''),
       bookingDate: new FormControl(''),
       guests: this.formBuilder.array([this.formBuilder.group({
-        name: new FormControl(''),
+        name: new FormControl('', { validators:[Validators.required]}),
         age: new FormControl('')
       })]),
       guestDetails: this.formBuilder.group({
-        guestName: new FormControl(''),
-        guestAddress: new FormControl(''),
-        mobileNumber: new FormControl(''),
-      })
+        guestName: new FormControl('', { validators:[Validators.required]}),
+        guestAddress: new FormControl('', {validators: [Validators.required]}),
+        mobileNumber: new FormControl('', { updateOn: 'blur' }),
+      }),
+      tnc: new FormControl(false, {validators: [Validators.requiredTrue]})
+    })
+    this.getBookingData()
+    this.bookingForm.valueChanges.subscribe((data) => { 
+      console.log(data)
     })
   }
 
+  getBookingData(){
+    this.bookingForm.patchValue({
+      roomId: '1',
+      guestEmail: 'h@g.co',
+  })
+}
+
   addBooking() {
     console.log(this.bookingForm.getRawValue());
+    this.bookingForm.reset()
   }
 
   addGuest() {
@@ -57,6 +71,10 @@ export class BookingComponent implements OnInit {
         age: new FormControl('')
       })
     )
+  }
+
+  deleteGuest(i: number) {
+    this.guests.removeAt(i)
   }
 
   addPassport() {
